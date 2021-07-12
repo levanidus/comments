f31e2536c3c620522dc7fabeea9e4cbcb7ffee86
rogebor <levanidus@gmail.com>
2021-07-12 12:13:56 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,4 @@ cd ${HOME}/comments
 cp .env.example .env
 docker-compose build phpfpm
 docker run --rm -i -t -v ${HOME}/comments:/var/www comments_phpfpm /bin/bash -c "composer install && npm install"
-docker-compose up -d
-docker exec -ti app_phpfpm bash -c "php artisan migrate && php artisan key:generate"
+
